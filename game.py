c96681860543554eae8eba5f247f3bd4c9aeb78e
Егор <@>
2026-01-11 19:46:03 +0300

    0.1.0

--- game.py
+++ game.py
@@ -1,8 +1,6 @@
 import arcade
-from arcade.gui import UIManager, UIAnchorLayout, UIBoxLayout
-
+from arcade.gui import UIManager, UILabel, UIBoxLayout
 import json
-
 import province
 
 
@@ -14,45 +12,48 @@ class Game(arcade.View):
         self.gui_camera = arcade.camera.Camera2D()
 
         self.all_provinces = arcade.SpriteList()
-        self.player_sprite = None
 
-        self.pan_speed = 5.0
-        self.keys = {
-            arcade.key.W: False,
-            arcade.key.S: False,
-            arcade.key.A: False,
-            arcade.key.D: False,
-        }
+        self.pan_speed = 10.0
+        self.keys = {key: False for key in (arcade.key.W, arcade.key.S, arcade.key.A, arcade.key.D)}
 
         self.manager = None
+        self.info_box = None
+        self.info_label = None
 
     def on_show_view(self):
         arcade.set_background_color(arcade.color.LIGHT_BLUE)
 
-        with open("provinces.json", mode="r", encoding="utf-8") as file:
+        with open("provinces.json", "r", encoding="utf-8") as file:
             data = json.load(file)
-            for i in data:
-                prov = province.Province(f"images/{i}.png", data[i]["center_x"], data[i]["center_y"], data[i]["color"])
+            for name in data:
+                prov = province.Province(
+                    f"images/{name}.png",
+                    data[name]["center_x"],
+                    data[name]["center_y"],
+                    data[name]["color"],
+                    name,
+                    data[name]["resource"]
+                )
+                prov.set_hit_box(prov.texture.hit_box_points)
                 self.all_provinces.append(prov)
 
-        self.world_camera.position = (self.window.width / 2, self.window.height / 2)
-
-        self.manager = UIManager(self.window)
+        self.manager = UIManager()
         self.manager.enable()
 
-        self.anchor_layout = UIAnchorLayout()
-        self.box_layout = UIBoxLayout(vertical=True, space_between=28)
-        self.setup_widgets()
+        self.info_label = UILabel(
+            text="",
+            text_color=arcade.color.BLACK,
+            font_size=12,
+            multiline=True,
+            width=250
+        )
 
-        self.anchor_layout.add(self.box_layout, anchor_x="center", anchor_y="top", align_y=-170)
-        self.manager.add(self.anchor_layout)
+        self.info_box = UIBoxLayout(vertical=True)
+        self.info_box.add(self.info_label)
 
-    def setup_widgets(self):
-        ...
+        self.manager.add(self.info_box.with_space_around(left=10, bottom=10))
 
-    def on_hide_view(self):
-        if self.manager:
-            self.manager.disable()
+        self.info_box.visible = False
 
     def on_update(self, delta_time: float):
         x, y = self.world_camera.position
@@ -64,19 +65,24 @@ class Game(arcade.View):
             x -= self.pan_speed
         if self.keys[arcade.key.D]:
             x += self.pan_speed
+
         self.world_camera.position = (x, y)
 
-    def on_key_press(self, symbol: int, modifiers: int):
-        if symbol in self.keys:
-            self.keys[symbol] = True
+    def on_mouse_press(self, x, y, button, modifiers):
+        world_x, world_y, _ = self.world_camera.unproject((x, y))
 
-    def on_key_release(self, symbol: int, modifiers: int):
-        if symbol in self.keys:
-            self.keys[symbol] = False
+        for prov in self.all_provinces:
+            if prov.collides_with_point((world_x, world_y)):
+                text = (
+                    f"Province: {prov.name}\n"
+                    f"Color: {prov.color}\n"
+                    f"Resources: {prov.resource}"
+                )
+                self.info_label.text = text
+                self.info_box.visible = True
+                return
 
-    def on_mouse_scroll(self, x: int, y: int, scroll_x: int, scroll_y: int):
-        zoom_factor = 1.1 if scroll_y > 0 else 1 / 1.1
-        self.world_camera.zoom *= zoom_factor
+        self.info_box.visible = False
 
     def on_draw(self):
         self.clear()
@@ -84,6 +90,17 @@ class Game(arcade.View):
         self.world_camera.use()
         self.all_provinces.draw()
 
-        if self.manager:
-            self.gui_camera.use()
-            self.manager.draw()
+        self.gui_camera.use()
+        self.manager.draw()
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol in self.keys:
+            self.keys[symbol] = True
+
+    def on_key_release(self, symbol, modifiers):
+        if symbol in self.keys:
+            self.keys[symbol] = False
+
+    def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
+        zoom_factor = 1.1 if scroll_y > 0 else 1 / 1.1
+        self.world_camera.zoom *= zoom_factor
